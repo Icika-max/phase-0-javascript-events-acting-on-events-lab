46e3f3245c93bb2b467ccac82c6f8658df1e3cb1
icika-max <icika-max>
2022-11-21 00:08:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function moveDodgerleft(){
+  const leftNumbers = dodger.style.replace("px", "");
+  const left =perseInt(leftNumbers,10);
+  if (left>0) {
+    dodger.style.left= '${left -1'}
+}
+document.addEventListener("keydown", function(e){
+  if (e.key=== "arrowleft") {
+    moveDodgerleft();
+  }
+});
+function moveDodgerRight()
+{
+  const rightNumbers =dodger.style.left.replace( "px", "");
+  const right = parseInt( rightNumbers,10);
+  if ( right>0)
+  {
+    dodger.style.left= '${right +1}px';
+  }
+
+
+}
+document.addEventListener("keydown", function(e)
+{
+if (e.key==="ArrowRight")
+{ moveDodgerRight()}
+})
